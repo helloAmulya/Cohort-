fdecc88af4521b43ec98c56498294426fda7b3d5
helloAmulya <internatomix@gmail.com>
2025-07-16 12:26:40 +0530

    users table created

--- week10/Postgres2/src/index.ts
+++ week10/Postgres2/src/index.ts
@@ -7,3 +7,18 @@ const client = new Client({
 })
 
 client.connect()
+
+
+
+
+async function createUsersTable() {
+    const result = await client.query(`
+        CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        )
+        `)
+}   
