88e096e00f5a6ad4c4c3585eda5e4cb5e642a3d2
helloAmulya <internatomix@gmail.com>
2025-07-29 21:08:02 +0530

    cloudflare wrangler setup

--- week11/my-app/src/index.ts
+++ week11/my-app/src/index.ts
@@ -11,10 +11,32 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return Response.json({
+// 			message: "Hello"
+// 		});
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return Response.json({
-			message: "Hello"
-		});
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+		console.log(request.method)
+
+
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		}
+		else {
+			return Response.json({
+				message: "you did not sent a get  request"
+			});
+		}
 	},
-} satisfies ExportedHandler<Env>;
+
+}
