049100be35311d34827243816dec4ac77c949d86
helloAmulya <internatomix@gmail.com>
2025-08-02 02:57:57 +0530

    modern landing page css

--- practice/reprac/src/App.jsx
+++ practice/reprac/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import ModalPopBox from "./components/ModalPopBox";
+import Herosection from "./components/Herosection";
+
 
 function App() {
-  const [count, setCount] = useState(0);
+return (
+    <div className="h-screen bg-mainBG"> 
+      {/* <ModalPopBox /> */}
+      <Herosection/>
 
-  return (
-    <div>
-      <ModalPopBox />
     </div>
   );
 }
