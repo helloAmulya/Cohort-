d8f443da2aceaa15a13f1aa65b08aab05cefeade
helloAmulya <internatomix@gmail.com>
2025-08-10 06:48:18 +0530

    prisma accelerate and db

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -5,5 +5,25 @@ const app = new Hono()
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+app.post('/api/v1/signup', (c) => {
+  return c.text('signup!')
+})
+app.post('/api/v1/signin', (c) => {
+  return c.text('signin!')
+})
+app.post('/api/v1/blog', (c) => {
+  return c.text('post blog!')
+})
+app.put('/api/v1/blog', (c) => {
+  return c.text('edit blog!')
+})
+app.get('/api/v1/blog', (c) => {
+  return c.text('get blog!')
+})
+
+
+
+
 
 export default app
+
