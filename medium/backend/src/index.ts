c77686d7c8000cfd9ba732356e74769ea3daa826
helloAmulya <internatomix@gmail.com>
2025-08-15 06:10:35 +0530

    adding signup route with hono jwt

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,27 +1,44 @@
 import { Hono } from 'hono'
-
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { jwt } from 'hono/jwt'
+import { JwtVariables } from 'hono/jwt'
 
 // const prisma = new PrismaClient().$extends(withAccelerate())
 
-const app = new Hono()
+// const app = new Hono()
+//  so in hono we need to pass the generic to get the env and all  
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string // now i can use the env 
+    // Variables: JwtVariables // will not use this for now
+    JWT_SECRET:string
+  }
+}>
 
-app.post('/api/v1/signup', (c) => {
+//  if we want to ignore something (bad practice) somehow, so we use the @ts-ignore , this will ignore any ts related issues
+
+app.post('/api/v1/signup', async (c, next) => {
 
   const prisma = new PrismaClient({
-    datasourceUrl: env.DATABASE_URL,
+    datasourceUrl: c.env.DATABASE_URL, // now no error 
   }).$extends(withAccelerate())
-  return c.text('Hello world')
+
+  //  to get data/body inside hono we use the 'c'
+  const body = await c.req.json();
+
+  const jwtMiddleware = jwt({
+    secret: c.env.JWT_SECRET,
+  })
+
+  await prisma.user.create({
+    data: body
+  })
+
 
 })
 
 
-
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
 app.post('/api/v1/signup', (c) => {
   return c.text('signup!')
 })
