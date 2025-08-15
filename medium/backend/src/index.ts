a314ab6e402e61cdb078961bca91676bc8b4640c
helloAmulya <internatomix@gmail.com>
2025-08-15 06:43:59 +0530

    added routes & env variable changes

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,50 +1,77 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { jwt } from 'hono/jwt'
-import { JwtVariables } from 'hono/jwt'
+import { decode, sign, verify, jwt } from 'hono/jwt'
 
-// const prisma = new PrismaClient().$extends(withAccelerate())
-
-// const app = new Hono()
 //  so in hono we need to pass the generic to get the env and all  
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string // now i can use the env 
-    // Variables: JwtVariables // will not use this for now
-    JWT_SECRET:string
+    PRISMA_DATABASE_URL: string // now i can use the env 
+    JWT_SECRET: string
   }
 }>
 
-//  if we want to ignore something (bad practice) somehow, so we use the @ts-ignore , this will ignore any ts related issues
+//  if we want to ignore something (bad practice) somehow, so we use the '@ts-ignore' , this will ignore any ts related issues
 
 app.post('/api/v1/signup', async (c, next) => {
 
   const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL, // now no error 
+    datasourceUrl: c.env.PRISMA_DATABASE_URL, // now no error 
   }).$extends(withAccelerate())
 
   //  to get data/body inside hono we use the 'c'
   const body = await c.req.json();
 
-  const jwtMiddleware = jwt({
-    secret: c.env.JWT_SECRET,
+
+  const secret = c.env.JWT_SECRET
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  })
+  const token = sign({ id: user.id }, secret)
+  return c.json({
+    token: token
   })
 
-  await prisma.user.create({
-    data: body
+
+})
+
+
+app.post('/api/v1/signin', async (c, next) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.PRISMA_DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const secret = c.env.JWT_SECRET
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email
+    }
+  });
+
+  if (!user) {
+    c.status(403)
+    return c.json({
+      error: "user not foundx",
+    })
+  }
+
+  const token = await sign({ id: user.id }, secret)
+  return c.json({
+    token: token
   })
 
 
+
 })
 
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('signup!')
-})
-app.post('/api/v1/signin', (c) => {
-  return c.text('signin!')
-})
+
+
 app.post('/api/v1/blog', (c) => {
   return c.text('post blog!')
 })
