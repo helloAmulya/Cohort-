094a8d1ceefd1b9a0489e13c5b52214f3f94f404
helloAmulya <internatomix@gmail.com>
2025-08-22 04:57:19 +0530

    fix: added zod validation schema to common as module

---
+++ medium1/common/src/index.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+export const signupInput = z.object({
+    username: z.string().trim().toLowerCase().max(10),
+    email: z.string().trim().toLowerCase().email(),
+    password: z.string().min(5).max(14),
+
+});
+
+export const signinInput = z.object({
+    email: z.string().trim().toLowerCase().email(),
+    password: z.string().min(5).max(14),
+});
+
+export const createBlogInput = z.object({
+    title: z.string().min(1, "Title is required"),
+    content: z.string().min(1, "Content is required"),
+    thumbnail: z.string().url().optional(),
+});
+
+export const updateBlogInput = z.object({
+    title: z.string().min(1, "Title is required").optional(),
+    content: z.string().min(1, "Content is required").optional(),
+    thumbnail: z.string().url().optional()
+})
+
+
+// types
+export type SignupSchema = z.infer<typeof signupInput>;
+export type SigninSchema = z.infer<typeof signinInput>;
+export type CreateBlogSchema = z.infer<typeof createBlogInput>;
+export type UpdateBlogSchema = z.infer<typeof updateBlogInput>;
