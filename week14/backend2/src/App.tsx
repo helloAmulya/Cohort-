56e33746007863ec5e8278efe4d33d85c2e376b9
helloAmulya <internatomix@gmail.com>
2025-09-22 23:24:14 +0530

    fix: resolved code issues

---
+++ week14/backend2/src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState({
+    email: "",
+    name: ""
+  })
+  useEffect(() => {
+    axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+      .then(res => {
+        setLoading(false);
+        setData(res.data);
+      })
+  }, [])
+
+  if (loading) {
+    return "Loading..."
+  }
+  return (
+    <>
+      <div>
+        {data.email} {" | "}
+        {data.name}
+      </div>
+    </>
+  )
+}
+
+export default App
